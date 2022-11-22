89558ececb88880a4ac057aef1a2063e1e43112f
Sermixdev <sermixdev@gmail.com>
2022-11-22 23:07:55 +0100

    Starting with js, alHacerClick function

--- calculator/js/index.js
+++ calculator/js/index.js
@@ -1,3 +1,8 @@
-$("document").ready(()=>{
-    $("document").html()
-});$("do")
+const frame = document.getElementById("frame")
+console.log(frame);
+// FUNCION SE EJECUTA CUANDO OCURRE EL EVENTO DE CLICK DESDE EL DOM
+function alHacerClick(valor){
+    if (valor === '8'){
+        frame.value += valor;
+    }
+}
