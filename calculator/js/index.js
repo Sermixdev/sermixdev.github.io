c2978736f0953f3d5d13852df7f8d9ee13894a21
Sermixdev <sermixdev@gmail.com>
2022-11-23 21:06:27 +0100

    Added final version of calc logic

--- calculator/js/index.js
+++ calculator/js/index.js
@@ -1,18 +1,74 @@
 const frame = document.getElementById("frame")
-console.log(frame);
 // FUNCION QUE SE EJECUTA CUANDO OCURRE EL EVENTO DE CLICK DESDE EL DOM
 function alHacerClick(valor){
-    if (!frame.value) {
-        frame.value = "";
-    }
-    frame.value += valor;
+    if (empiezaConOperador(valor) === false){
+        if (typeof valor === "string"){
+            if (valor === "="){
+                calcular(frame.value);
+            }else if (operadorRepetido(frame.value) === false ){
+                frame.value += valor;
+            }
+        }else{
+            frame.value += valor;
+        }
     }
 
+
+}
 // FUNCION PARA CALCULAR EN EL CASO DE QUE INTRODUZCA INTRODUCIR
 function calcular(operacion){
-    const resultado = eval(operacion);
-    frame.value = resultado;
+    try{
+        console.log(operacion);
+        if (isNaN(operacion)){
+            frame.value = "¿Estás segur@ de eso?";
+            setTimeout(()=>{
+                frame.value = "";
+            }, 2000);
+        }else if (operacion === null || operacion  === ""){
+            frame.value = "¿Se te ha escapado el enter?";
+            setTimeout(()=>{
+                frame.value = "";
+            }, 2000);
+        }else if(operacion.substring(operacion.length-1) !== "+" &&
+        operacion.substring(operacion.length-1) !== "-" &&
+        operacion.substring(operacion.length-1) !== "/" &&
+        operacion.substring(operacion.length-1) !== "*"){
+            let resultado = eval(operacion);
+            if (esDecimal(resultado) === true){
+                frame.value = resultado.toFixed(3);
+            }else{
+                frame.value = resultado;
+            }
+            
+        }
+    }catch(e){
+        alert(e);
+        frame.value = "";
+    }
+    
 }
+// CONTROLAR QUE NO INTRODUZCA EN PRIMER LUGAR OPERADORES * / 
+function empiezaConOperador(valor){
+    if ((frame.value == "" || frame.value == null) && (valor === "/" || valor === "*")){
+        return true;
+    }else{
+        return false;
+    }
+}
+// FUNCION QUE COMPRUEBA QUE EL ULTIMO VALOR INTRODUCIDO NO SEA UN OPERADOR REPETIDO
+function operadorRepetido (comprobar){
+    if(comprobar.substring(comprobar.length-1) === "+" ||
+        comprobar.substring(comprobar.length-1) === "-" ||
+        comprobar.substring(comprobar.length-1) === "/" ||
+        comprobar.substring(comprobar.length-1) === "*" ||
+        comprobar.substring(comprobar.length-1) === "."){
+            return true;
+    }else{
+        return false;
+    }
+}
+
+
 // FUNCION QUE CONTROLA QUE TIPO DE CARACTER HA INTRODUCIDO EL USUARIO
 document.addEventListener("keydown", valorIntroducido);
 function valorIntroducido(i){
@@ -44,18 +100,32 @@ function valorIntroducido(i){
 
     // CONTROL DE LOS OPERADORES
     if (i.key === "+"){
-        frame.value += "+";
+        if (operadorRepetido(frame.value) == false){
+            frame.value += "+";
+        }
     }else if (i.key === "-"){
-        frame.value += "-";
+            if (operadorRepetido(frame.value) == false){
+                frame.value += "-";
+            }
     }else if (i.key === "*"){
-        frame.value += "*";
+        if (empiezaConOperador(i.key) === false){
+            if (operadorRepetido(frame.value) == false){
+                frame.value += "*";
+            }
+        }
     }else if (i.key === "/"){
-        frame.value += "/"
+        if (empiezaConOperador(i.key) === false){
+            if (operadorRepetido(frame.value) == false){
+                frame.value += "/";
+            }
+        }
     }
-
+    
     // CONTROL DEL DECIMAL
     if (i.key === "."){
-        frame.value += ".";
+        if (operadorRepetido(frame.value) == false){
+            frame.value += ".";
+        }
     }
 
     // CONTROL DE INTRODUCIR, LLAMA A LA FUNCION CALCULAR 
@@ -71,7 +141,10 @@ function valorIntroducido(i){
     // CONTROL DE RETROCESO, BORRA ULTIMO CARACTER INTRODUCIDO
     if (i.key === "Backspace") {
         const borrar = frame.value;
-        //remove the last element in the string
         frame.value = borrar.substring(0, frame.value.length - 1);
     }
+}
+
+function esDecimal(num){
+    return !!(num % 1); 
 }
